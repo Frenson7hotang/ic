ac1602cc06cf43cb6535ac2a7d0db4547f5d3715
Frenson7hotang <frensonpekanbaru2019@gmail.com>
2026-06-02 14:44:53 +0700

    lanjut

---
+++ resources/views/part/head.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="admin, dashboard">
+	<meta name="author" content="DexignZone">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Siandika - Dashboard">
+	<meta property="og:title" content="Siandika - Dashboard">
+	<meta property="og:description" content="Siandika - Dashboard">
+	<meta property="og:image" content="https://dompet.dexignlab.com/xhtml/social-image.png">
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- PAGE TITLE HERE -->
+	<title>Dompet : Payment Admin Template</title>
+	
+	<!-- FAVICONS ICON -->
+	<link rel="shortcut icon" type="image/png" href="{{ asset('load/images/favicon.png') }}">
+	
+	<link href="{{ asset ('load/vendor/jquery-nice-select/css/nice-select.css') }}" rel="stylesheet">
+	<link rel="stylesheet" href="{{ asset('load/vendor/nouislider/nouislider.min.css') }}">
+	<!-- Style css -->
+    <link href="{{ asset('load/css/style.css') }}" rel="stylesheet">
+	
+</head>
---
+++ resources/views/part/script.blade.php
@@ -0,0 +1,18 @@
+<!-- Required vendors -->
+    <script src="{{ asset('load/vendor/global/global.min.js') }}"></script>
+	<script src="{{ asset('vendor/chart.js/Chart.bundle.min.js') }}"></script>
+	<script src="{{ asset('vendor/jquery-nice-select/js/jquery.nice-select.min.js') }}"></script>
+	
+	<!-- Apex Chart -->
+	<script src="{{ asset('vendor/apexchart/apexchart.js') }}"></script>
+	<script src="{{ asset('vendor/nouislider/nouislider.min.js') }}"></script>
+	<script src="{{ asset('vendor/wnumb/wNumb.js') }}"></script>
+	
+	<!-- Dashboard 1 -->
+	<script src="{{ asset('load/js/dashboard/dashboard-1.js') }}"></script>
+
+    <script src="{{ asset('load/js/custom.min.js') }}"></script>
+	<script src="{{ asset('load/js/dlabnav-init.js') }}"></script>
+	<script src="{{ asset('load/js/demo.js') }}"></script>
+    <script src="{{ asset('load/js/styleSwitcher.js') }}"></script>
+	
